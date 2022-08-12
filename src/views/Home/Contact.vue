993e8939f096e72ae490773f311f9fdbb93237cc
Felipe Moura <felipemimoura@gmail.com>
2022-08-12 12:04:25 -0300

    feat: create initial page

---
+++ src/views/Home/Contact.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div class="flex justify-center w-full">
+
+    <div class="flex flex-col items-center w-4/5 max-w-6xl my-16">
+      <h2 class=" text-4xl font-black text-gray-800 text-center ">Alguma dúvida?</h2>
+      <p class="font-regular text-center text-lg text-gray-800">
+        Quer saber melhor como funciona e quais são os preços?
+      </p>
+      <div class="mt-10">
+        <a href="mailto:" class="px-6 py-3 mt-10 text-white rounded-full bg-brand-main focus:outline-none">
+          Nos mande um e-mail!
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
